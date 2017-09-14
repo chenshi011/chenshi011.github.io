042ec3f92a2c48c7877ab9c02935ec97303997ac
chenshi011 <chenshi011@163.com>
2017-09-14 19:11:51 +0800

    Add files via upload

--- wedding/js/functions.js
+++ wedding/js/functions.js
@@ -4,22 +4,19 @@ var clientWidth = $(window).width();
 var clientHeight = $(window).height();
 
 $(function () {
-    // setup garden
-	$loveHeart = $("#loveHeart");
-	var offsetX = $loveHeart.width() / 2;
-	var offsetY = $loveHeart.height() / 2 - 55;
-    $garden = $("#garden");
-    gardenCanvas = $garden[0];
-	gardenCanvas.width = $("#loveHeart").width();
-    gardenCanvas.height = $("#loveHeart").height()
-    gardenCtx = gardenCanvas.getContext("2d");
-    gardenCtx.globalCompositeOperation = "lighter";
-    garden = new Garden(gardenCtx, gardenCanvas);
-	
-    // renderLoop
-    setInterval(function () {
-        garden.render();
-    }, Garden.options.growSpeed);
+	pageResponse({
+        selectors : '.page',     //模块选择器，使用querySelectorAll的方法
+        mode : 'contain',     // auto || contain || cover ，默认模式为auto 
+        width : '320',      //输入页面的宽度，只支持输入数值，默认宽度为320px
+        height : '504'      //输入页面的高度，只支持输入数值，默认高度为504px
+    })
+	pageResponse({
+        selectors : '#loveHeart',     
+        mode : 'contain',     
+        width : '670',      
+        height : '625'      
+    })
+    
 });
 
 $(window).resize(function() {
@@ -27,13 +24,111 @@ $(window).resize(function() {
     var newHeight = $(window).height();
     if (newWidth != clientWidth && newHeight != clientHeight) {
         location.replace(location);
+		
     }
 });
 
+function pageResponse(opt) {
+    var ua = navigator.userAgent,
+        wp = ua.match(/Windows Phone ([\d.]+)/),
+        android = ua.match(/(Android);?[\s\/]+([\d.]+)?/),
+
+        // 设备宽高初始比例
+        dw = document.documentElement.clientWidth,
+        dh = document.documentElement.clientHeight,
+        ds = dw / dh,
+
+        // 页面宽高初始比例
+        pw = opt.width || 320,
+        ph = opt.height || 504,
+        ps = pw / ph,
+
+        // 调用的选择器
+        pd = document.querySelectorAll(opt.selectors),
+        i = pd.length,
+
+        // 核心代码：页面缩放比例
+        sm = opt.mode || "auto",
+        or = opt.origin || "left top 0",
+        sn = (sm == "contain") ? (ds > ps ? dh / ph : dw / pw) : (sm == "cover") ? (ds < ps ? dh / ph : dw / pw) : dw / pw; 
+
+    //样式模板 auto || contain || cover
+    function template(mode, obj, num) {
+        var _o = obj.style;
+        _o.width = pw + "px";
+        _o.height = ph + "px";
+        _o.webkitTransformOrigin = or;
+        _o.transformOrigin = or;
+        _o.webkitTransform = "scale(" + num + ")";
+        _o.transform = "scale(" + num + ")";
+        // 兼容android 2.3.5系统下body高度不自动刷新的bug
+        if (mode == "auto" && android) {
+            document.body.style.height = ph * num + "px";
+        } else
+        if (mode == "contain" || mode == "cover") {
+            _o.position = "absolute";
+            _o.left = (dw - pw) / 2 + "px";
+            _o.top = (dh - ph) / 2 + "px";
+            _o.webkitTransformOrigin = "center center 0";
+            _o.transformOrigin = "center center 0";
+            // 阻止默认滑屏事件
+            if (wp) {
+                document.body.style.msTouchAction = "none";
+            } else {
+                document.ontouchmove = function(e) {
+                    e.preventDefault()
+                }
+            }
+        }
+		
+		if (obj.id == "loveHeart") {
+			initGarden(pw,ph, num);
+			adjustCodePosition(pw * num / 20);
+		}
+    }
+
+    //运行
+    while (--i >= 0) {
+        template(sm, pd[i], sn);
+    }
+	
+}
+/*  使用方法
+ *  window.onload = window.onresize = function(){
+ *      pageResponse({
+ *          selectors : '输入类名', //模块的类名
+ *          mode : 'contain',    // auto || contain || cover 
+ *          width : '320',     //默认宽320px 
+ *          height : '504',     //默认高504px
+ *          origin : 'center center 0'     //缩放中心点，可选，在contain和cover模式下无效，默认为"left top 0"
+ *      })
+ *   }
+ */
+
+
+function initGarden(w,h,scale){
+	// setup garden
+    $garden = $("#garden");
+    gardenCanvas = $garden[0];
+	gardenCanvas.width = w * scale;
+    gardenCanvas.height = h * scale;
+    gardenCtx = gardenCanvas.getContext("2d");
+    gardenCtx.globalCompositeOperation = "lighter";
+    garden = new Garden(gardenCtx, gardenCanvas);
+	offsetX = w * scale / 2;
+	offsetY = h * scale / 2 - 55 * scale;
+	mGardenScale = scale;
+    // renderLoop
+    setInterval(function () {
+        garden.render();
+    }, Garden.options.growSpeed);
+
+}
+
 function getHeartPoint(angle) {
 	var t = angle / Math.PI;
-	var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
-	var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+	var x = 19.5 * (16 * Math.pow(Math.sin(t), 3)) * mGardenScale;
+	var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t)) * mGardenScale;
 	return new Array(offsetX + x, offsetY + y);
 }
 
@@ -122,10 +217,10 @@ function adjustWordsPosition() {
 	$('#words').css("left", $("#garden").position().left + 70);
 }
 
-function adjustCodePosition() {
+function adjustCodePosition(offsetX) {
 	//$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
-	$('#code').css("top", $("#garden").position().top + 395);
-	$('#code').css("left", offsetX - 100);
+	$('#code').css("top", $("#content").position().top + $("#content").height() / 2);
+	$('#code').css("left", offsetX);
 }
 
 function showLoveU() {
